57d940c8e90368372869a8d5f77c168582f03003
Nick Brandt <nickbrandt2012@aol.com>
2018-12-03 12:46:27 -0500

    [Spotify] Had typos in 'https://' for axios calls

--- routes/spotify.js
+++ routes/spotify.js
@@ -8,7 +8,7 @@ module.exports = {
         let payload = Buffer(clientId + ':' + clientSecret).toString('base64');
         let auth = 'Basic ' + payload;
 
-        return axios.post('https//:accounts.spotify.com/api/token',
+        return axios.post('https://accounts.spotify.com/api/token',
             qs.stringify({ grant_type: "client_credentials" }), { headers: {
                 'Authorization': auth,
                 'Content-Type': 'application/x-www-form-urlencoded'
@@ -19,7 +19,7 @@ module.exports = {
 
 // Get all albums from the Artist passed and randomly select one and return it
     getRandomAlbum: async function (artistId, accessToken) {
-        return axios.get('https//:api.spotify.com/v1/artists/' + artistId + '/albums', {
+        return axios.get('https://api.spotify.com/v1/artists/' + artistId + '/albums', {
             headers: {
                 'Authorization': 'Bearer ' + accessToken,
             }
@@ -29,7 +29,7 @@ module.exports = {
 // Get all tracks from the Album passed and randomly select one and return it
     getRandomTrackFromAlbum: async function (albumId, accessToken) {
 
-        return axios.get('https//:api.spotify.com/v1/albums/' + albumId + '/tracks', {
+        return axios.get('https://api.spotify.com/v1/albums/' + albumId + '/tracks', {
             headers: {
                 'Authorization': 'Bearer ' + accessToken,
             }
