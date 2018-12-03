19efef718096d178755dee97a8650a40467885cd
Nick Brandt <nickbrandt2012@aol.com>
2018-12-03 00:26:44 -0500

    Update spotify.js exports

--- routes/spotify.js
+++ routes/spotify.js
@@ -1,41 +1,41 @@
 const axios = require('axios');
 
+module.exports = {
+
 // Get an API token using the provided ID and Secret
-function getAccessToken(clientId, clientSecret) {
-    let payload = Buffer(clientId + ':' + clientSecret).toString('base64');
-    let auth = 'Basic ' + payload;
-    let postBody = { grant_type: "client_credentials"};
+    getAccessToken: async function (clientId, clientSecret) {
+        let payload = Buffer(clientId + ':' + clientSecret).toString('base64');
+        let auth = 'Basic ' + payload;
+        let postBody = {grant_type: "client_credentials"};
 
-    return axios.post('https:accounts.spotify.com/api/token', postBody, {
-        headers: {
-            'Authorization': auth,
-            'Content-Type': 'x-www-form-urlencoded'
-        }
-    });
+        return axios.post('https:accounts.spotify.com/api/token', postBody, {
+            headers: {
+                'Authorization': auth,
+                'Content-Type': 'x-www-form-urlencoded'
+            }
+        });
 
-}
+    },
+
 // Get all albums from the Artist passed and randomly select one and return it
-function getRandomAlbum(artistId, accessToken) {
-    return axios.get('https:api.spotify.com/v1/artists/'+ artistId +'/albums', {
-        headers: {
-            'Authorization': 'Bearer ' + accessToken,
-        }
-    });
-}
+    getRandomAlbum: async function (artistId, accessToken) {
+        return axios.get('https:api.spotify.com/v1/artists/' + artistId + '/albums', {
+            headers: {
+                'Authorization': 'Bearer ' + accessToken,
+            }
+        });
+    },
 
 // Get all tracks from the Album passed and randomly select one and return it
-function getRandomTrackFromAlbum(albumId, accessToken) {
+    getRandomTrackFromAlbum: async function (albumId, accessToken) {
 
-    return axios.get('https:api.spotify.com/v1/albums/'+ albumId +'/tracks', {
-        headers: {
-            'Authorization': 'Bearer ' + accessToken,
-        }
-    });
+        return axios.get('https:api.spotify.com/v1/albums/' + albumId + '/tracks', {
+            headers: {
+                'Authorization': 'Bearer ' + accessToken,
+            }
+        });
 
-}
+    }
 
-// Export the helper functions
-module.exports.getAccessToken = getAccessToken();
-module.exports.getRandomAlbum = getRandomAlbum();
-module.exports.getRandomTrackFromAlbum = getRandomTrackFromAlbum();
+};
 
