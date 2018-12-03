928538b7d79ba4f12b393fb8b457885d5f730aa2
Nick Brandt <nickbrandt2012@aol.com>
2018-12-03 00:29:17 -0500

    Unpromisify the postSongResponse method

--- routes/thug.js
+++ routes/thug.js
@@ -175,23 +175,22 @@ function postPictureResponse(id, name, res) {
 // Send a spotify link as a message
 function postSongResponse(id, name, res){
 
-    getRandomSong().then(track => {
-        let outgoing = JSON.stringify({
-            bot_id: config.thugbot.bot_id,
-            text: '@' + name + "bump it luv" + track['spotify'],
-            attachments: [
-                {
-                    type: 'mentions',
-                    user_ids: [id],
-                    loci: [
-                        [0, 1 + name.length]
-                    ]
-                }
-            ]
+    let track = getRandomSong();
+    console.log(track);
+    let outgoing = JSON.stringify({
+        bot_id: config.thugbot.bot_id,
+        text: '@' + name + "bump it luv" + track['spotify'],
+        attachments: [
+            {
+                type: 'mentions',
+                user_ids: [id],
+                loci: [
+                    [0, 1 + name.length]
+                ]
+            }]
         });
 
-        sendResponse(outgoing, res);
-    })
+    sendResponse(outgoing, res);
 
 }
 
